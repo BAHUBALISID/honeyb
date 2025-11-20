77caa3fa12f129f4307d7030030ec76a8bdaf15e
Sidharth <128839129+BAHUBALISID@users.noreply.github.com>
2025-11-21 03:13:31 +0530

    Update banner.php

--- banner.php
+++ banner.php
@@ -2,15 +2,20 @@
 global $bold, $magenta, $lblue, $yellow, $white, $cln;
 
 echo $bold . $magenta . "
-    ██╗  ██╗ ██████╗ ███╗   ██╗███████╗██╗   ██╗██████╗ 
-    ██║  ██║██╔═══██╗████╗  ██║██╔════╝╚██╗ ██╔╝██╔══██╗
-    ███████║██║   ██║██╔██╗ ██║█████╗   ╚████╔╝ ██████╔╝
-    ██╔══██║██║   ██║██║╚██╗██║██╔══╝    ╚██╔╝  ██╔══██╗
-    ██║  ██║╚██████╔╝██║ ╚████║███████╗   ██║   ██████╔╝
-    ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═════╝ 
+       /~~\               ,,,c(((((((((((((((((/
+      /~~c \.         .vv)))))))))))))))))))\``
+          G_G__   ,,(((KKKK//////////////'
+        ,Z~__ '@,gW@@AKXX~MW,gmmmz==m_.
+       iP,dW@!,A@@@@@@@@@@@@@@@A` ,W@@A\c
+       ]b_.__zf !P~@@@@@*P~b.~+=m@@@*~ g@Ws.
+          ~`    ,2W2m. '\[ ['~~c'M7 _gW@@A`'s
+            v=XX)====Y-  [ [    \c/*@@@*~ g@@i
+           /v~           !.!.     '\c7+sg@@@@@s.
+          //              'c'c       '\c7*X7~~~~
+         ]/                 ~=Xm_       '~=(Gm_.
 " . $cln;
 
 echo $bold . $lblue . "\n            Comprehensive Security Tool Suite\n";
-echo $bold . $yellow . "            Version 1.0 | Multi-Tool Integration\n";
-echo $bold . $white . "        CATEYE + Advanced Security Scanning Tools\n\n" . $cln;
+echo $bold . $yellow . "            MADE BY SID07 | Multi-Tool Integration\n";
+echo $bold . $white . "          Advanced Security Scanning Tools\n\n" . $cln;
 ?>
