672b975d565fc225267bf886dca99dfc3775168d
Sidharth <128839129+BAHUBALISID@users.noreply.github.com>
2025-11-21 02:43:33 +0530

    Create colors.php

---
+++ config/colors.php
@@ -0,0 +1,17 @@
+<?php
+// Terminal Colors
+$cln = "\e[0m";
+$bold = "\e[1m";
+$red = "\e[91m";
+$green = "\e[92m";
+$yellow = "\e[93m";
+$blue = "\e[94m";
+$magenta = "\e[95m";
+$cyan = "\e[96m";
+$white = "\e[97m";
+
+// Background Colors
+$bg_red = "\e[41m";
+$bg_green = "\e[42m";
+$bg_yellow = "\e[43m";
+?>
